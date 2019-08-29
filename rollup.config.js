1eee54de2bed78d0a38116664f0a279d663d7c89
Michael Budde <mbu@jobindex.dk>
2019-08-29 09:23:55 +0200

    Add build output that does not contain polyfills

--- rollup.config.js
+++ rollup.config.js
@@ -5,45 +5,49 @@ import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 import replace from 'rollup-plugin-replace';
 
+function babelPresets(polyfill) {
+  const presetOptions = {
+    targets: {
+      "ie": 11
+    }
+  };
+  if (polyfill) {
+    presetOptions.useBuiltIns = "usage";
+    presetOptions.corejs = "2";
+  }
+  return [["@babel/preset-env", presetOptions]];
+}
+
 const configs = [];
 for (let minified of [true, false]) {
-  for (let output of [{
-    file: `dist/js/dawa-autocomplete2.${minified ? 'min.' : ''}js`,
-    format: 'umd',
-    name: 'dawaAutocomplete'
-  }, {
-    file: `dist/js/dawa-autocomplete2.es.${minified ? 'min.' : ''}js`,
-    format: 'es'
-  }]) {
-    const config = {
-      input: 'src/dawa-autocomplete2.js',
-      plugins: [
-        resolve(),
-        replace({
-          'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        commonjs({}),
-        babel({
-          "presets": [
-            [
-              "@babel/preset-env",
-              {
-                "useBuiltIns": "usage",
-                "targets": {
-                  "ie": "11"
-                },
-                corejs: '2'
-              }
-            ]
-          ],
-          ignore: ['node_modules']
-        }),
-        ...(minified ? [terser({})] : [])
-      ],
-      output,
+  for (let polyfill of [true, false]) {
+    for (let output of [{
+      file: `dist/js/dawa-autocomplete2.${polyfill ? '' : 'nopolyfill.'}${minified ? 'min.' : ''}js`,
+      format: 'umd',
+      name: 'dawaAutocomplete'
+    }, {
+      file: `dist/js/dawa-autocomplete2.es.${polyfill ? '' : 'nopolyfill.'}${minified ? 'min.' : ''}js`,
+      format: 'es'
+    }]) {
+      const config = {
+        input: 'src/dawa-autocomplete2.js',
+        plugins: [
+          resolve(),
+          replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+          }),
+          commonjs({}),
+          babel({
+            presets: babelPresets(polyfill),
+            ignore: ['node_modules']
+          }),
+          ...(minified ? [terser({})] : [])
+        ],
+        output,
 
-    };
-    configs.push(config);
+      };
+      configs.push(config);
+    }
   }
 }
 export default configs;
